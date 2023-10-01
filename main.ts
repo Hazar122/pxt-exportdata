660b6f7ab403014427c439222f73fa2f064ad659
Hazar122 <67770254+Hazar122@users.noreply.github.com>
2023-10-02 00:59:41 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,4 +3,9 @@ namespace JSON {
     export function stringToJson(text: string) {
         return JSON.parse(text)
     }
+    //% block
+    export function intToString(num: number): string {
+        return num.toString();
+    }
+
 }
