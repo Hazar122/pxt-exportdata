747ceece4c01e04f9e8318cd2cf170f9f8ae7b63
Hazar122 <67770254+Hazar122@users.noreply.github.com>
2023-10-02 00:42:08 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,7 @@
 // Define a custom block
 // @param text - the string to convert to JSON
 // @param returns - the JSON object
-// @category JSON
-function stringToJson(text: string): any {
-    return JSON.parse(text);
+// @category Custom
+function stringToJson (text: string) {
+    return JSON.parse(text)
 }
-
-
